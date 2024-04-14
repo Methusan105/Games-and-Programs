e6d9e9327f3e3567929a41fbfcc26d13a5260f56
Methusan105 <methusan@hotmail.no>
2024-04-14 12:28:03 +0200

    1212

--- upload asset files.py
+++ upload asset files.py
@@ -1,8 +1,8 @@
 import os
-release_file_path = r"C:\Users\Methu\Downloads\Compressed\Update.1.3.6.zip"
+release_file_path = r"C:\Users\Methu\Downloads\Compressed\Update\Update.1.4.0.zip"
 
 # Assuming you want to iterate from 1 to 12
-for i in range(1, 6):
+for i in range(1, 18):
     file_number = f"{i:03d}"  # Format the number with leading zeros
     file_path = f'{release_file_path}.{file_number}'
     
