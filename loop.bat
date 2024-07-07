d1a7af3bb7020c71ae7dc8ad39e9ee0a659b2200
Methusan105 <methusan@hotmail.no>
2024-07-07 22:17:23 +0200

    1212

--- loop.bat
+++ loop.bat
@@ -1,9 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 
-for /L %%i in (1,1,31) do (
-    set "url=https://github.com/Methusan105/Games-and-Programs/releases/download/HZD/Horizon.Zero.Dawn-%%i.bin"
-    "C:\Program Files (x86)\Internet Download Manager\IDMan.exe" /a /d "!url!" from -%%i.bin to -%%i.bin
+for /L %%i in (1,1,28) do (
+    set "url=https://github.com/Methusan105/Games-and-Programs/releases/download/GOT/Ghost.of.Tsushima.Director.s.Cut.zip.%%03d"
+    "C:\Program Files (x86)\Internet Download Manager\IDMan.exe" /a /d "!url!"
 )
 
 endlocal
